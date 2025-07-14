4b4c189d34b29043fa214f2636239b347447103b
Mukund <mukundsharma456@gmail.com>
2025-07-15 00:00:17 +0530

    ff

--- main.py
+++ main.py
@@ -1,6 +1,2 @@
 print("Hello, World!")
 # This is a simple Python script that prints "Hello, World!" to the console.
-# You can run this script using Python 3.
-def
-main():
-    print("Hello, World!")
